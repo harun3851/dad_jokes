5a3d0dae61cbf3a583f51a9ef6d50671c5ebec8a
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-02-01 00:18:07 +0200

    try and cache method added

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const btnEl=document.getElementById("btn");
 const apiKey="xZK/ggwkdQt3NtdqJICDmg==G0Bo5wPD5AaoveDG";
-const option={
+const jokeEl=document.getElementById("joke");
+const options={
     method:"GET",
     headers:{
-        "X-Api-Key":apiKey
+        "X-Api-Key":apiKey,
     }
 }
-function getJoke(){
-    console.log("clicked");
-
-}
-
-btnEl.addEventListener("click",getJoke)
+const apiURL="https://api.api-ninjas.com/v1/dadjokes?limit=1";
+async function getJoke() {
+    try {
+      jokeEl.innerText = "Updating...";
+      btnEl.disabled = true;
+      btnEl.innerText = "Loading...";
+      const response = await fetch(apiURL, options);
+      const data = await response.json();
+  
+      btnEl.disabled = false;
+      btnEl.innerText = "Tell me a joke";
+  
+      jokeEl.innerText = data[0].joke;
+    } catch (error) {
+      jokeEl.innerText = "An error happened, try again later";
+      btnEl.disabled = false;
+      btnEl.innerText = "Tell me a joke";
+      console.log(error);
+    }
+  }
+  
+  btnEl.addEventListener("click", getJoke);
+  
